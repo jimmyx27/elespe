7c8394becdc367b70dc5f4cfb3327dfa4b9f1fcf
zuli <jimmyx27@gmail.com>
2025-12-13 18:15:10 -0500

    commit

--- main.go
+++ main.go
@@ -50,15 +50,15 @@ type Verse struct {
 
 type Message struct {
 	Type       string         `json:"type"`
-	Content    *string        `json:"content"`
-	Verse      *Verse         `json:"verse,omitempty"`
-	Number     *int           `json:"number,omitempty"`
-	Total      *int           `json:"total,omitempty"`
+	Content    string         `json:"content"`
+	Verse      Verse          `json:"verse,omitempty"`
+	Number     int            `json:"number,omitempty"`
+	Total      int            `json:"total,omitempty"`
 	Stats      *Stats         `json:"stats,omitempty"`
 	Books      []string       `json:"books,omitempty"`
 	Progress   map[string]int `json:"progress,omitempty"`
 	Favorites  []Verse        `json:"favorites,omitempty"`
-	IsFavorite *bool          `json:"isFavorite,omitempty"`
+	IsFavorite bool           `json:"isFavorite,omitempty"`
 }
 
 var bible struct {
