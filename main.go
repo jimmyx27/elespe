eb60837db5f0c22353e166562e0204e68079db9b
zuli <jimmyx27@gmail.com>
2025-12-15 18:33:14 -0500

    commit

--- main.go
+++ main.go
@@ -330,12 +330,14 @@ func groupVersesByBook(verses []Verse) {
 var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
 
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	log.Println("Connecting websocket")
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("Upgrade error:", err)
 		return
 	}
 	defer conn.Close()
+	log.Println("Websocket connected")
 
 	uid := r.URL.Query().Get("uid")
 	if uid == "" {
